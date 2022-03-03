45a89f411cd4d6c34ce293e59c80d262c555091a
Lipka <krzysztof.lipka@capgemini.com>
2022-03-03 11:47:12 +0100

    remove previous project and refractor

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -27,6 +27,7 @@ namespace studygroups.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
+            _logger.LogInformation("test info");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
